3246e11b2750a8a115d8ce65bb65e1ad5103e92e
iamnelsonmartins <nelsonjmartins@gmail.com>
2020-10-26 23:34:43 +0000

    folder structure update and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,21 +5,28 @@ module.exports = {
   entry: './src/js/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
   },
   module: {
     rules: [
       { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
       { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+      {
+        test: /\.(png|svg|jpg|gif|ico)$/,
+        loader: 'file-loader',
+        options: { name: 'images/[name].[ext]' },
+      },
     ],
   },
   mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html',
+      favicon: 'src/assets/favicon.ico',
     }),
   ],
   devServer: {
     open: 'Google Chrome',
+    port: 9009,
   },
 }
